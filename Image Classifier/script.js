6c7fa39f8d1ab1e9b1a8a1a594846e7229914778
anujdutt9 <anuj.dutt9@gmail.com>
2019-03-09 19:11:12 -0500

    Update

---
+++ Image Classifier/script.js
@@ -0,0 +1,20 @@
+// Script to load the MobileNet model and setup for Inference
+
+// Define a Model Variable
+let model;
+
+// Function to Load the Model, get the image and make predictions
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+
+  // Make a prediction through the model on our image.
+  const imgEl = document.getElementById('img');
+  const result = await net.classify(imgEl);
+  console.log(result);
+}
+
+app();
